15bcd704ca1467864def6d5b6bc1aa587b55a470
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-05-30 23:53:08 +0900

    login

--- soptoon/routes/user/userRouter.js
+++ soptoon/routes/user/userRouter.js
@@ -19,7 +19,8 @@ router.post('/signup', async(req, res) => {
             res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.USER_SELECT_FAIL));
         } else {
             getAllUserResult.forEach((user) => {
-                if(user.id == req.body.id)
+                console.log(user)
+                if(user.userId == req.body.id)
                 {
                     count += 1;
                     res.status(200).send(defaultRes.successFalse(statusCode.BAD_REQUEST, resMessage.USER_ALREADY_EXISTS));
@@ -40,5 +41,38 @@ router.post('/signup', async(req, res) => {
         }
     }
 })
+
+router.post('/login', async(req, res) => {
+    try{
+        const getUserWithSameIdQuery = 'SELECT * FROM user WHERE userId = ?';
+        let resultUser = await db.queryParam_Parse(getUserWithSameIdQuery, [req.body.id] );
+        if(resultUser.length == 0)
+        {
+            res.status(200).send(defaultRes.successFalse(statusCode.BAD_REQUEST, resMessage.ID_MISS_MATCH));
+        }
+        else{
+            const salt = resultUser[0].salt;
+            const password = await crypto.pbkdf2(req.body.password, salt, 1000, 32, 'SHA512');
+            if(resultUser[0].password == password.toString('base64'))
+            {
+                const User = {
+                    userIdx: resultUser[0].userIdx,
+                    id: resultUser[0].userId,
+                    name: resultUser[0].userName
+                }
+                res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.LOGIN_SUCCESS, User));
+            }
+            else{
+                res.status(200).send(defaultRes.successFalse(statusCode.BAD_REQUEST, resMessage.PASSWORD_MISS_MATCH));
+            } 
+        }
+    }
+    catch(err){
+            console.log(err)
+            res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.USER_SELECT_FAIL));
+        }
+    }
+    
+)
 
 module.exports = router;
